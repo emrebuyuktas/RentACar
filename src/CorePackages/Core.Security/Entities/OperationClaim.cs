761197f41cdb15c98a9d6c64c05faa9bc2e00cb0
Emre Büyüktaş <emrebuyuktas@assatekno.com>
2024-03-07 19:18:45 +0300

    feat: add core security

---
+++ src/CorePackages/Core.Security/Entities/OperationClaim.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class OperationClaim : Entity<int>
+{
+    public string Name { get; set; }
+    public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; }
+
+    public OperationClaim(string name)
+    {
+        Name = name;
+    }
+}
---
+++ src/CorePackages/Core.Security/Entities/UserOperationClaim.cs
@@ -0,0 +1,17 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class UserOperationClaim : Entity<int>
+{
+    public int UserId { get; set; }
+    public int OperationCalimId { get; set; }
+    public virtual User User { get; set; }
+    public virtual OperationClaim OperationClaim { get; set; }
+
+    public UserOperationClaim(int userId, int operationCalimId)
+    {
+        UserId = userId;
+        OperationCalimId = operationCalimId;
+    }
+}
